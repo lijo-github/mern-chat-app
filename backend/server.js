2558b23edf48fc6d1f6070f55303be20a53cb5dd
lijo_george <lijogeorge389@gmail.com>
2024-04-13 03:19:46 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import  express  from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from 'path';
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -9,6 +10,7 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,6 +21,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname,"frontend", "dist" , "index.html"));
+})
 // app.get("/", (req,res)=>{
 //   //* root route http://localhost:5000/
 //   res.send("hello world!!");
